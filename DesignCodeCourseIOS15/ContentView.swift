00e3e213417147b08a64c9b4d7ddd3c03e5974d6
BrotskyS <sergiypik.work@gmail.com>
2022-04-01 21:57:02 +0300

    Initial Commit

---
+++ DesignCodeCourseIOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeCourseIOS15
+//
+//  Created by Sergiy Brotsky on 01.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeCourseIOS15/DesignCodeCourseIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseIOS15App.swift
+//  DesignCodeCourseIOS15
+//
+//  Created by Sergiy Brotsky on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
